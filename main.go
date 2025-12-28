821502c011c2ae92788ffb223c7e257ef3f9cee9
Shizzy-ctrl <pawelgren462@gmail.com>
2025-12-28 17:27:18 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+type Config struct {
+	Quotes    []string `json:"quotes"`
+	ChannelID string   `json:"channel_id"`
+}
+
+var (
+	config     Config
+	configFile = "config.json"
+)
+
+func main() {
+	token := os.Getenv("DISCORD_TOKEN")
+	if token == "" {
+		log.Fatal("Brak tokena Discord! Ustaw zmienną DISCORD_TOKEN")
+	}
+
+	loadConfig()
+
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatal("Błąd tworzenia sesji:", err)
+	}
+
+	dg.AddHandler(messageCreate)
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	err = dg.Open()
+	if err != nil {
+		log.Fatal("Błąd otwierania połączenia:", err)
+	}
+	defer dg.Close()
+
+	fmt.Println("Bot działa! Naciśnij CTRL+C aby zakończyć.")
+
+	// Uruchom codzienne wysyłanie o 9:00
+	go scheduleDailyQuote(dg)
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+}
+
+func loadConfig() {
+	data, err := os.ReadFile(configFile)
+	if err != nil {
+		config = Config{
+			Quotes: []string{
+				"Wytrwałość to klucz do sukcesu.",
+				"Każdy dzień to nowa szansa.",
+				"Wierz w siebie i swoje możliwości.",
+			},
+			ChannelID: "",
+		}
+		saveConfig()
+		return
+	}
+	json.Unmarshal(data, &config)
+}
+
+func saveConfig() {
+	data, _ := json.MarshalIndent(config, "", "  ")
+	os.WriteFile(configFile, data, 0644)
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	content := strings.TrimSpace(m.Content)
+
+	if content == "!zlotamysl" || content == "!zm" {
+		sendRandomQuote(s, m.ChannelID)
+	} else if strings.HasPrefix(content, "!dodaj ") {
+		quote := strings.TrimPrefix(content, "!dodaj ")
+		config.Quotes = append(config.Quotes, quote)
+		saveConfig()
+		s.ChannelMessageSend(m.ChannelID, "✅ Dodano nową złotą myśl!")
+	} else if strings.HasPrefix(content, "!usun ") {
+		numStr := strings.TrimPrefix(content, "!usun ")
+		var num int
+		fmt.Sscanf(numStr, "%d", &num)
+		if num > 0 && num <= len(config.Quotes) {
+			config.Quotes = append(config.Quotes[:num-1], config.Quotes[num:]...)
+			saveConfig()
+			s.ChannelMessageSend(m.ChannelID, "✅ Usunięto złotą myśl!")
+		} else {
+			s.ChannelMessageSend(m.ChannelID, "❌ Nieprawidłowy numer!")
+		}
+	} else if content == "!lista" {
+		if len(config.Quotes) == 0 {
+			s.ChannelMessageSend(m.ChannelID, "Brak złotych myśli!")
+			return
+		}
+		var msg strings.Builder
+		msg.WriteString("**📜 Lista złotych myśli:**\n\n")
+		for i, q := range config.Quotes {
+			msg.WriteString(fmt.Sprintf("%d. %s\n", i+1, q))
+		}
+		s.ChannelMessageSend(m.ChannelID, msg.String())
+	} else if strings.HasPrefix(content, "!kanal ") {
+		channelID := strings.TrimPrefix(content, "!kanal ")
+		config.ChannelID = channelID
+		saveConfig()
+		s.ChannelMessageSend(m.ChannelID, "✅ Ustawiono kanał dla codziennych myśli!")
+	} else if content == "!pomoc" {
+		help := `**🌟 Złote Myśli Bot - Komendy:**
+
+!zlotamysl lub !zm - Wyświetl losową złotą myśl
+!dodaj <tekst> - Dodaj nową złotą myśl
+!usun <numer> - Usuń złotą myśl (podaj numer z listy)
+!lista - Pokaż wszystkie złote myśli
+!kanal <ID> - Ustaw kanał dla codziennych myśli o 9:00
+!pomoc - Pokaż tę pomoc`
+		s.ChannelMessageSend(m.ChannelID, help)
+	}
+}
+
+func sendRandomQuote(s *discordgo.Session, channelID string) {
+	if len(config.Quotes) == 0 {
+		s.ChannelMessageSend(channelID, "Brak złotych myśli! Dodaj je komendą !dodaj")
+		return
+	}
+	quote := config.Quotes[rand.Intn(len(config.Quotes))]
+	s.ChannelMessageSend(channelID, fmt.Sprintf("✨ **Złota Myśl:** ✨\n\n*%s*", quote))
+}
+
+func scheduleDailyQuote(s *discordgo.Session) {
+	for {
+		now := time.Now()
+		next := time.Date(now.Year(), now.Month(), now.Day(), 9, 0, 0, 0, now.Location())
+		if now.After(next) {
+			next = next.Add(24 * time.Hour)
+		}
+		duration := next.Sub(now)
+
+		time.Sleep(duration)
+
+		if config.ChannelID != "" {
+			sendRandomQuote(s, config.ChannelID)
+		}
+	}
+}

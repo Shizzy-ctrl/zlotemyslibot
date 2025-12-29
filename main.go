95a27dbc74565df65f2c8489fcbb2a034fcf473d
Shizzy-ctrl <pawelgren462@gmail.com>
2025-12-29 10:42:51 +0100

    zmiana godziny

--- main.go
+++ main.go
@@ -139,7 +139,7 @@ func scheduleDailyQuote(s *discordgo.Session) {
 
 	for t := range ticker.C {
 		now := t.In(loc)
-		if now.Hour() == 10 && now.Minute() == 35 {
+		if now.Hour() == 10 && now.Minute() == 47 {
 			if config.ChannelID != "" {
 				sendRandomQuote(s, config.ChannelID)
 			}
